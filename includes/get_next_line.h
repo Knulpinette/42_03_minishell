24ba7155d284683eef5195d5ee82e5849caac8f2
Sara Vieira <s.cvieira@sapo.pt>
2022-02-16 17:32:38 +0100

    adding gnl, should be changed once we add libft

---
+++ includes/get_next_line.h
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: svieira <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/02/24 11:49:00 by svieira           #+#    #+#             */
+/*   Updated: 2021/03/08 12:20:33 by svieira          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdlib.h>
+# include <unistd.h>
+
+size_t	ft_strlen(const char *s);
+char	*ft_strdup(const char *s1);
+char	*ft_strjoin(const char *s1, const char *s2);
+char	*ft_strchr(const char *s, int c);
+char	*ft_substr(const char *s, unsigned int start, size_t len);
+int		ft_memdel(void **ptr);
+int		get_next_line(int fd, char **line);
+
+#endif
