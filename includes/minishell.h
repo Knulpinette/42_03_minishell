10393d9dbf459ef0acafefb0ef13834da0368080
Knulpinette <ophelie.surcouf@gmail.com>
2022-02-16 14:21:00 +0100

    added comment for debug_rule

--- includes/minishell.h
+++ includes/minishell.h
@@ -1,6 +1,15 @@
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
+// You can delete that comment once you read it.
+// Idea is that with this you can write stuff like DEBUG(print_tokens(get_minishell(NULL));) and that line will only be happening if you "make debug".
+// Which is pretty practical I think while we work to verify nothing breaks. Macros are so powerful. It's crazy.
+#ifdef DEBUG_MODE
+#  define DEBUG(x) x
+# else
+#  define DEBUG(x)
+#endif
+
 # include <unistd.h>
 # include <stdio.h>
 # include <stdlib.h>
