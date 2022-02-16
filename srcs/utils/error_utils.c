cd7e74e64a24cf72c3c8096bd6b3bc07a27485b7
Knulpinette <ophelie.surcouf@gmail.com>
2022-02-16 13:04:38 +0100

    added some comments for clarity

--- srcs/utils/error_utils.c
+++ srcs/utils/error_utils.c
@@ -5,7 +5,7 @@ int		error_and_exit(t_error code)
 	t_minishell *minishell;
 
 	minishell = get_minishell(NULL);
-	error_message(code);
+	error_message(code); // later on, maybe add the program_pathname stored in struct to print a clearer error
 	if (minishell)
 		free_minishell(minishell);
 	exit(EXIT_FAILURE);
@@ -13,6 +13,7 @@ int		error_and_exit(t_error code)
 
 void	error_message(t_error code)
 {
+	// we could print the program name here
 	if (MEMORY_FAIL) //in the meantime before linking libft. 
 		printf("%s\n", strerror(errno)); // don't forget to print on STDERR_FILENO
 }
--- srcs/utils/main_utils.c
+++ srcs/utils/main_utils.c
@@ -1,5 +1,8 @@
 #include "minishell.h"
 
+/* cool concept taken from Michele who didn't use any global variables. 
+ This allow us to store the pointer of minishell once and be able to
+ reaccess it any point in our code, without passing the struct. 	*/
 t_minishell		*get_minishell(t_minishell *minishell)
 {
 	static t_minishell	*pointer_to_minishell = NULL;
