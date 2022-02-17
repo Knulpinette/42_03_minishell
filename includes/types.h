8fe1374e731cdcd7ce9831902d29762f450355ca
Knulpinette <ophelie.surcouf@gmail.com>
2022-02-17 16:25:42 +0100

    added comment on error codes

--- includes/types.h
+++ includes/types.h
@@ -6,7 +6,7 @@ typedef struct 	s_minishell
 	char* coconut;
 }				t_minishell;
 
-// ask Michele for explanation on how they did it and why
+/* see https://www.cyberciti.biz/faq/linux-bash-exit-status-set-exit-statusin-bash/ */
 # define EXIT_FILE_NOT_FOUND 127
 
 // error codes that we can use for the error function and making the whole thing clearer.
