14eb08bf9b620b14ae0eb9cef381a7869af79cd7
Knulpinette <ophelie.surcouf@gmail.com>
2022-02-21 23:36:22 +0100

    should we go for lists or arrays?

---
+++ includes/struct_def.h
@@ -0,0 +1,56 @@
+#ifndef STRUCT_DEF_H
+# define STRUCT_DEF_H
+
+/* PARSING MACROS */
+# define SPACE ' '
+# define PIPE '|'
+# define ENV_VARIABLE '$'
+# define QUOTE '\''
+# define DBL_QUOTE '"'
+# define REDIR_IN '<'
+# define REDIR_OUT '>'
+# define DELIMITER "<<" // see if that makes sense eventually
+# define APPEND_MODE ">>" // same
+
+typedef struct 	s_command_table
+{
+	char	*cmd;
+	char	**flags;
+	char	*cmd_path; // this will be the concenated "right"/correct path (or at least where we'll store the paths we want to test to see if they can be executed)
+	int		infile_fd;
+	char	*infile;
+	int		outfile_fd;
+	char	*outfile;
+	bool	delimiter;
+	char	*delim_arg;
+	int		mode; /* OVERWRITE, APPEND */
+	char	**args; // * ou ** ?
+
+}				t_cmd_table;
+
+typedef	enum	e_redir_mode // for readable instructions when you change the mode according to >>
+{
+	OVERWRITE, // default IS overwrite, right ? (here overwrite = 0)
+	APPEND,
+}				t_redir_mode;
+
+/* see https://www.cyberciti.biz/faq/linux-bash-exit-status-set-exit-statusin-bash/ */
+# define EXIT_FILE_NOT_FOUND 127
+
+// error codes that we can use for the error function and making the whole thing clearer.
+typedef enum 	e_error_codes
+{
+	ERR_NO_PRINT,
+	MEMORY_FAIL,
+	WRONG_ARGC,
+}				t_error;
+
+typedef struct 	s_minishell
+{
+	char		*prompt;
+	char		**envp_paths; // this is the split with all the paths from envp. We only need to get it once in the beginning. It will always be the same for the whole program.
+	t_cmd_table	*cmd_table; // this is a list of cmd tables ?
+
+}				t_minishell;
+
+#endif
