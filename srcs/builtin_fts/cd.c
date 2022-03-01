a68ad98db67b63210b670e105e9a40039c845efe
Sara Vieira <s.cvieira@sapo.pt>
2022-03-01 19:43:39 +0100

    working

--- srcs/builtin_fts/cd.c
+++ srcs/builtin_fts/cd.c
@@ -5,6 +5,7 @@
  * 1) no argument is given => change to HOME
  * 2) - => change to OLDPWD
  * 3) relative or absolute path are given => change to path
+ * chdir can receive both relative or absolute paths - so smart ✨
  */
 
 int	cd(t_cmd_table *cmd)
@@ -12,43 +13,39 @@ int	cd(t_cmd_table *cmd)
 	t_minishell	*minishell;
 	char		*cwd;
 	char		*new_cwd;
+	int			exit_code;
 	int			i;
+	extern char	**environ;
 
 	minishell = get_minishell(NULL);
 	cwd = NULL;
-	cwd = getcwd(cwd, 0); // or without cwd = ?
+	cwd = getcwd(cwd, 0);
 	DEBUG(printf("cwd: %s\n", cwd));
-	DEGUB(printf("arg: %s\n", cmd->cmd_args[0]));
+	DEBUG(printf("cmd: %s arg: %s\n", cmd->cmd_name, cmd->cmd_args[0]));
+	
 	if (!cmd->cmd_args[0])
-	{
-		i = 0;
-		while (minishell->envp[i] && ft_strncmp(minishell->envp[i], "HOME=", 5))
-			i++;
-		new_cwd = ft_strdup(minishell->envp[i] + 5);
-	}
-	else if (ft_strncmp(cmd->cmd_args[0], "-", 1))
-	{
-		i = 0;
-		while (minishell->envp[i] && ft_strncmp(minishell->envp[i], "OLDPWD=", 7))
-			i++;
-		new_cwd = ft_strdup(minishell->envp[i] + 7);
-	}
+		new_cwd = getenv("HOME");
+	else if (ft_strncmp(cmd->cmd_args[0], "-", 1) == 0)
+		new_cwd = getenv("OLDPWD");
 	else	
-		new_cwd = ft_strdup(cmd->cmd_args[0]);// nops, need to think a lot about this
+		new_cwd = cmd->cmd_args[0];// nops, need to think a lot about this
 	DEBUG(printf("new cwd: %s\n", new_cwd));
-	DEBUG(printf("before changing dir\n"));
-	chdir(new_cwd); 
+
+	exit_code = chdir(new_cwd); // if exit_code != 0 there's an error
+	
+	free(cwd);
+	cwd = NULL;
+	cwd = getcwd(cwd, 0); // to get the absolute path
+	printf("cwd after chdir: %s\n", cwd);
 	i = 0;
-	while (minishell->envp[i] && ft_strncmp(minishell->envp[i], "PWD=", 4))
+	while (minishell->envp[i] && ft_strncmp(environ[i], "PWD=", 4))
 		i++;
-	printf("%s\n", minishell->envp[i]);	
+	printf("environ %s\n", environ[i]);
+	DEBUG(printf("%s\n", getenv("PWD")));	
 	// update PWD in envp with args
-	i = 0;
-	while (minishell->envp[i] && ft_strncmp(minishell->envp[i], "OLDPWD=", 7))
-		i++;
-	printf("%s\n", minishell->envp[i]);	
+
+	DEBUG(printf("%s\n", getenv("OLDPWD")));	
 	// update OLDPWD in envp with cwd
 	free(cwd);
-	free(new_cwd);
 	return (0); // should be exit code
 }
