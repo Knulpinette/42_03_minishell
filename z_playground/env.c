3f89ef8194299de7543138aaecf785bab933171f
Sara Vieira <s.cvieira@sapo.pt>
2022-03-15 19:23:03 +0000

    reaching sad conclusions

--- z_playground/env.c
+++ z_playground/env.c
@@ -10,8 +10,8 @@
  *    Seems like there is an envp somewhere anyway...
  * 2) Neither this misterious env nor the envp argument change automatically
  *    when they should (for example, when chdir function is used)
- * 3) If we always need to set it up manually, then we might as well create our
- *    own structure for it...
+ * 3) If we modify envp arg manually, getenv will retrieve the updated value
+ * 4) If we create something else out of envp and update it, it won't
  */
 
 typedef struct s_env {
@@ -61,22 +61,30 @@ void	free_env_lst(t_list **env_lst)
 	ft_lstclear(env_lst, &del_env_content);
 }
 
+char	*get_env_name(t_list *env_lst)
+{
+	return (((t_env *)env_lst->content)->name);
+}
+
 int	main(int ac, char **av, char **envp)
 {
 	(void)ac;
 	(void)av;
-	int		i;
 	t_list	*env_lst;
+	t_list	*to_change;
 
-	i = 0;
 	env_lst = init_env_lst(envp);
-	//	i++;
-	//envp[i] = strdup("PWD=heyo");
-	printf("%s=", ((t_env *)env_lst->content)->name);
+	/*printf("%s=", ((t_env *)env_lst->content)->name);
 	printf("%s\n", ((t_env *)env_lst->content)->value);
 	printf("%s=", ((t_env *)env_lst->next->content)->name);
-	printf("%s\n", ((t_env *)env_lst->next->content)->value);
-	setenv("PWD", "/Users/svieira/", 1);
+	printf("%s\n", ((t_env *)env_lst->next->content)->value);*/
+	to_change = env_lst;
+	while (to_change && ft_strncmp(get_env_name(to_change), "PWD", 3))
+		to_change = to_change->next;
+	printf("%s=", get_env_name(to_change)); 
+	free(((t_env *)to_change->content)->value);
+	((t_env *)to_change->content)->value = ft_strdup("/Users/svieira/");
+	//setenv("PWD", "/Users/svieira/", 1);
 	printf("%s\n", getenv("PWD"));
 	free_env_lst(&env_lst);
 	return (0);
