9bdbd471599a196ecae77722d81cc29b3c32f466
Knulpinette <ophelie.surcouf@gmail.com>
2022-03-15 11:32:48 +0000

    modified nothing

--- srcs/core_utils/main_utils.c
+++ srcs/core_utils/main_utils.c
@@ -3,6 +3,7 @@
 /* cool concept (singleton) taken from Michele who didn't use any global variables. 
  This allow us to store the pointer of minishell once and be able to
  reaccess it any point in our code, without passing the struct. 	*/
+ 
 t_minishell		*get_minishell(t_minishell *minishell)
 {
 	static t_minishell	*pointer_to_minishell = NULL;
