c3ec1f08b4824343575e81f09ad6ac360e0dc529
Sara Vieira <s.cvieira@sapo.pt>
2022-03-15 18:52:48 +0000

    fixed memory allocation issue

--- z_playground/env.c
+++ z_playground/env.c
@@ -19,23 +19,24 @@ typedef struct s_env {
 	char			*value;
 }				t_env;
 
-t_env	new_env_content(char *var)
+t_env	*new_env_content(char *var)
 {
 	int		i;
-	t_env	env_var;
+	t_env	*env_var;
 
 	i = 0;
 	while (var[i] && var[i] != '=')
 		i++;
-	env_var.name = ft_substr(var, 0, i);
-	env_var.value = ft_strdup(getenv(env_var.name));
+	env_var = (t_env *)malloc(sizeof(t_env));
+	env_var->name = ft_substr(var, 0, i);
+	env_var->value = ft_strdup(getenv(env_var->name));
 	return (env_var);
 }
 
-t_list	*init_env(char **envp)
+t_list	*init_env_lst(char **envp)
 {
 	int		i;
-	t_env	env_var;
+	t_env	*env_var;
 	t_list	*env_lst;
 
 	i = 0;
@@ -43,10 +44,7 @@ t_list	*init_env(char **envp)
 	while (envp[i])
 	{
 		env_var = new_env_content(envp[i]);
-		// should I allocate memory to env_var or the list?
-		// wondering because in the list it's type void
-		ft_lstadd_back(&env_lst, ft_lstnew((void *)&env_var));
-		printf("new item added\n");
+		ft_lstadd_back(&env_lst, ft_lstnew((void *)env_var));
 		i++;
 	}
 	return (env_lst);
@@ -56,13 +54,11 @@ void	del_env_content(void *env_var)
 {
 	free(((t_env *)env_var)->name);
 	free(((t_env *)env_var)->value);
-	printf("deleted content\n");
 }
 
-void	free_env(t_list **env)
+void	free_env_lst(t_list **env_lst)
 {
-	ft_lstclear(env, &del_env_content);
-	printf("deleted list\n");
+	ft_lstclear(env_lst, &del_env_content);
 }
 
 int	main(int ac, char **av, char **envp)
@@ -70,14 +66,18 @@ int	main(int ac, char **av, char **envp)
 	(void)ac;
 	(void)av;
 	int		i;
-	t_list	*env;
+	t_list	*env_lst;
 
 	i = 0;
-	env = init_env(envp);
+	env_lst = init_env_lst(envp);
 	//	i++;
 	//envp[i] = strdup("PWD=heyo");
+	printf("%s=", ((t_env *)env_lst->content)->name);
+	printf("%s\n", ((t_env *)env_lst->content)->value);
+	printf("%s=", ((t_env *)env_lst->next->content)->name);
+	printf("%s\n", ((t_env *)env_lst->next->content)->value);
 	setenv("PWD", "/Users/svieira/", 1);
 	printf("%s\n", getenv("PWD"));
-	free_env(&env);
+	free_env_lst(&env_lst);
 	return (0);
 }
