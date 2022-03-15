e1d279c83640f641d7a586561677943231a887d3
Sara Vieira <svieira@student.42lisboa.com>
2022-03-15 12:48:03 +0000

    adding vim's swap files

--- z_playground/env.c
+++ z_playground/env.c
@@ -1,24 +1,28 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <unistd.h>
 #include <string.h>
 
 /*
  * Conclusions:
- * 1) I don't need envp as arg from main to be able to use getenv.
+ * 1) I don't need envp as arg from main to be able to use getenv not setenv.
  *    Seems like there is an envp somewhere anyway...
+ * 2) Neither this misterious env nor the envp argument change automatically
+ *    when they should (for example, when chdir function is used)
+ * 3) If we always need to set it up manually, then we might as well create our
+ *    own structure for it...
  */
 
 
-int	main(int ac, char **av, char **envp)
+int	main(void)
 {
-	(void)ac;
-	(void)av;
 	int	i;
 
 	i = 0;
-	while (envp[i] && strncmp(envp[i], "PWD", 3))
-		i++;
-	envp[i] = strdup("PWD=heyo");
+	//while (envp[i] && strncmp(envp[i], "PWD", 3))
+	//	i++;
+	//envp[i] = strdup("PWD=heyo");
+	chdir("/Users/svieira/");
 	printf("%s\n", getenv("PWD"));
 	return (0);
 }
