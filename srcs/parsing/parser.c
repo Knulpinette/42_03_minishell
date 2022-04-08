ab81a6d482c84959584dc0a70b48137d6a7c6bd3
Knulpinette <ophelie.surcouf@gmail.com>
2022-04-08 12:13:55 +0100

    reorganised

--- srcs/parsing/parser.c
+++ srcs/parsing/parser.c
@@ -2,7 +2,27 @@
 
 void	parse(char *line)
 {
+	t_minishell	*minishell;
+	int			i;
+
+	minishell = get_minishell(NULL);
 	lexer(line);
+	i = 0;
+	while (i < minishell->nb_cmds)
+	{
+		get_tokens_types(minishell->cmd_table[i].tokens, minishell->cmd_table[i].nb_tokens);
+		assign_tokens(&minishell->cmd_table[i]);
+
+		DEBUG(printf("_____\nprint redirs\n");)
+		DEBUG(print_redirs(minishell->cmd_table[i].redirs, minishell->cmd_table[i].nb_redirs);)
+		DEBUG(printf("_____\nprint tokens\n");)
+		DEBUG(print_tokens(minishell->cmd_table[i].tokens, minishell->cmd_table[i].nb_tokens);)
+		DEBUG(printf("_____\n");)
+		//DEBUG(printf("cmd = %s\n", cmd_table[i].cmd_name);)
+		//DEBUG(print_split(cmd_table[i].cmd_args);)
+
+		i++;
+	}
 	//PARSER (grammar)
 	//parser();
 	//VALIDATION (meaning)
--- srcs/parsing/tokens_types.c
+++ srcs/parsing/tokens_types.c
@@ -20,3 +20,48 @@ void	get_tokens_types(t_token *tokens, int nb_tokens)
 	}
 	return ;
 }	
+
+static int	get_type_count(t_token *tokens, int nb_tokens, t_token_type type)
+{
+	int count;
+	int	i;
+
+	count = 0;
+	i = 0;
+	while (i < nb_tokens)
+	{
+		if (tokens[i].type == type)
+			count++;
+		i++;
+	}
+	return (count);
+}
+
+static char	**get_args(t_token *tokens, int nb_tokens, t_token_type arg_type)
+{
+	int		i;
+	int		j;
+	int		nb_args;
+	char	**args;
+
+	i = 0;
+	j = 0;
+	nb_args = get_type_count(tokens, nb_tokens, arg_type);
+	args = calloc_or_exit(sizeof(char*), nb_args + 1);
+	while (i < nb_tokens)
+	{
+		if (tokens[i].type == arg_type)
+			args[j++] = tokens[i].text;
+		i++;
+	}
+	args[j] = 0;
+	return (args);
+}
+
+void		assign_tokens(t_cmd_table *cmd_table)
+{
+	cmd_table->cmd_name = cmd_table->tokens[0].text;
+	cmd_table->flags = get_args(cmd_table->tokens, cmd_table->nb_tokens, FLAG);
+	cmd_table->cmd_args = get_args(cmd_table->tokens, cmd_table->nb_tokens, WORD);
+	return ;
+}
