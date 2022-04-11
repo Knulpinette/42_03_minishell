62612076ac431b9a64991ca08fed4553758d74fa
Knulpinette <ophelie.surcouf@gmail.com>
2022-04-11 10:32:02 +0100

    fixed temporarily the segault when cmd_name is NULL

--- srcs/executor.c
+++ srcs/executor.c
@@ -13,18 +13,23 @@ int	execute(t_minishell *minishell)
 	i = 0;
 	while (i < minishell->nb_cmds)
 	{
-		if (ft_strncmp(minishell->cmd_table[i].cmd_name, "cd", 2) == 0)
-			minishell->exit_code = cd(&minishell->cmd_table[i], minishell);
-		else if (ft_strncmp(minishell->cmd_table[i].cmd_name, "pwd", 3) == 0)
-			minishell->exit_code = pwd(&minishell->cmd_table[i]);
-		else if (ft_strncmp(minishell->cmd_table[i].cmd_name, "env", 3) == 0)
-			minishell->exit_code = env(&minishell->cmd_table[i], minishell);
-		else if (ft_strncmp(minishell->cmd_table[i].cmd_name, "echo", 4) == 0)
-			minishell->exit_code = echo(&minishell->cmd_table[i]);
-		else if (ft_strncmp(minishell->cmd_table[i].cmd_name, "export", 6) == 0)
-			minishell->exit_code = export(&minishell->cmd_table[i], minishell);
-		// TODO build an actually executor
-		i++;
+		if (!minishell->cmd_table[i].cmd_name)
+			i++; // if cmd_variable is NULL is creates a problem. Not sure if we should always allocate an empty string to CMD anyways if it's empty or handle it here.
+		else
+		{
+			if (ft_strncmp(minishell->cmd_table[i].cmd_name, "cd", 2) == 0)
+				minishell->exit_code = cd(&minishell->cmd_table[i], minishell);
+			else if (ft_strncmp(minishell->cmd_table[i].cmd_name, "pwd", 3) == 0)
+				minishell->exit_code = pwd(&minishell->cmd_table[i]);
+			else if (ft_strncmp(minishell->cmd_table[i].cmd_name, "env", 3) == 0)
+				minishell->exit_code = env(&minishell->cmd_table[i], minishell);
+			else if (ft_strncmp(minishell->cmd_table[i].cmd_name, "echo", 4) == 0)
+				minishell->exit_code = echo(&minishell->cmd_table[i]);
+			else if (ft_strncmp(minishell->cmd_table[i].cmd_name, "export", 6) == 0)
+				minishell->exit_code = export(&minishell->cmd_table[i], minishell);
+			// TODO build an actually executor
+			i++;
+		}
 	}
 	return (minishell->exit_code);
 }
