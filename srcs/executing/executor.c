4e27167589ac37828350410559dfae01c4891ca3
Sara Vieira <s.cvieira@sapo.pt>
2022-04-18 19:48:17 +0100

    moving around code

--- srcs/executing/executor.c
+++ srcs/executing/executor.c
@@ -14,6 +14,16 @@ See if it still segfaults when proper executor is built!
  *
  * 2. REDIRECTIONS
  *	Closes previous fd_in/out if not stdin/out (so pipe and other redirection)
+ *	Opens the new one
+ *	If something happens we're out with exit_code 1
+ *
+ * 3. NO CMD
+ *	If there's no command, we're out with exit_code 0
+ *
+ * 4. BUILT-IN
+ *	If the function is built-in, call it and update exit_code
+ *
+ * 5. NON-BUILT-IN
  *
  * X. CLOSING
  *	If fd_in != stdin close it, if fd_out != stdout close it
@@ -24,6 +34,14 @@ See if it still segfaults when proper executor is built!
  *	- if open function returns an error, we display message and go to next command
  *	  as it might be because the file doesn't exist (pretty common error)
  */
+
+static void	close_for_next_cmd(t_cmd_table cmd)
+{
+	if (cmd.fd_in != 0)
+		close(cmd.fd_in);
+	if (cmd.fd_out != 1)
+		close(cmd.fd_out);
+}
 
 int	execute(t_minishell *minishell)
 {
@@ -41,10 +59,12 @@ int	execute(t_minishell *minishell)
 			minishell->cmd_table[i].fd_out = fd[1];
 			minishell->cmd_table[i + 1].fd_in = fd[0];
 		}
-		if ((minishell->exit_code = exec_redirs(minishell->cmd_table[i])))
+		if ((minishell->exit_code = exec_redirs(minishell->cmd_table[i]))
+			|| !minishell->cmd_table[i].cmd_name)
+		{
+			close_for_next_cmd(minishell->cmd_table[i])
 			continue;
-		if (!minishell->cmd_table[i].cmd_name)
-			continue;
+		}
 		// if it's a builtin function -> aux function that returns which function it is?
 		// execute built-in function:
 		// can it be more elegant than this, or... not really?
@@ -69,13 +89,7 @@ int	execute(t_minishell *minishell)
 		// error handling: if at some point something happens... still need to close stuff
 		// and update exit code
 
-		// close writing end of pipe and fd_out file if needed?
-		// close fd[1] and close fd[0] from previous command! how? fd_in! if fd_in != 0
-		// how to know if it's needed? has_pipe and has_redir booleans?
-		if (minishell->cmd_table[i].fd_in != 0)
-			close(minishell->cmd_table[i].fd_in);
-		if (minishell->cmd_table[i].fd_out != 1)
-			close(minishell->cmd_table[i].fd_out);
+		close_for_next_cmd(minishell->cmd_table[i]);
 		i++;
 	}
 	return (minishell->exit_code);
