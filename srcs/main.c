dfd684a1cc32064b2da82dfd7a272114e06204a4
Sara Vieira <s.cvieira@sapo.pt>
2022-04-26 10:55:34 +0100

    checking comments in main

--- srcs/main.c
+++ srcs/main.c
@@ -1,26 +1,15 @@
 #include "minishell.h"
 
-/* Executing
- * 1. Creating a child process per command
- * 2. Figure out how to use multiple pipes
- * 3. Execve or run built-in command, accordingly
- */
-
 int	main(int argc, char **argv, char **envp)
 {
 	int			fd;
 	char		*line;
 	t_minishell	*minishell;
-	int			exit_code; // TODO think about this
+	int			exit_code;
 
-	/* 1. assign signals to appropriate handlers
-	 * 2. infinite while loop
-	 *   a. get user input
-	 *   b. parse it
-	 *   c. execute it
-	 */
 	if (argc > 2)
 		error_and_exit(WRONG_ARGC);
+	// TODO assign signals to appropriate handlers
 	fd = 0;
 	if (argc == 2)
 		fd = open_or_exit(argv[1], O_RDONLY);
@@ -29,7 +18,7 @@ int	main(int argc, char **argv, char **envp)
 	{
 		line = get_instructions_line(argc != 2, fd);
 		if (!line)
-			return (0);
+			return (0); // shouldn't this be break? o.O what was I thinking?
 		exit_code = parse(line);
 		free(line);
 		if (exit_code != SYNTAX_ERROR)
