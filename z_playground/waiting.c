0d3e661c7a4611b7dac77206574c857a2965377c
Sara Vieira <s.cvieira@sapo.pt>
2022-04-26 19:45:48 +0100

    trying out the waiting and storing exit codes strategy

--- z_playground/waiting.c
+++ z_playground/waiting.c
@@ -2,30 +2,49 @@
 #include <unistd.h>
 #include <stdio.h>
 
+#define NB_KIDS 3
+
 int	main(void)
 {
 	int		i;
-	pid_t	pid;
+	pid_t	pid[NB_KIDS];
+	int		status;
+	int		exit_code[NB_KIDS];
+	int		j;
 
 	i = 0;
-	while (i < 3)
+	while (i < NB_KIDS)
 	{
-		pid = fork();
-		if (pid == -1)
+		pid[i] = fork();
+		if (pid[i] == -1)
 		{
-			printf("Houston, we have a fork problem\n");
+			printf("Houston, we have a problem\n");
 			return (1);
 		}
-		if (pid == 0)
+		if (pid[i] == 0)
 		{
-			printf("")
+			sleep(NB_KIDS - i);
+			printf("Child process %d\n", i);
+			exit(i);
 		}
-		else
+		else if (i == NB_KIDS - 1)
 		{
-			waitpid(pid, &status, 0); // synchronous, this is a problem
-			if (WIFEXITED(status))
-				minishell->exit_code = WEXITSTATUS(status);
-			DEBUG(printf("Exit code: %d\n", WEXITSTATUS(status)));
+			j = 0;
+			while (j < NB_KIDS)
+			{
+				waitpid(pid[j], &status, 0);
+				if (WIFEXITED(status))
+					exit_code[j] = WEXITSTATUS(status);
+				j++;
+			}
 		}
+		i++;
 	}
+	i = 0;
+	while (i < NB_KIDS)
+	{
+		printf("Exit code: %d\n", exit_code[i]);
+		i++;
+	}
+	return (exit_code[NB_KIDS - 1]);
 }
