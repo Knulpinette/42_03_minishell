6a502fb1bcd716b7708afe2878853ddd1887e6ed
Knulpinette <ophelie.surcouf@gmail.com>
2022-05-03 10:15:43 +0100

    patch up

--- srcs/interface.c
+++ srcs/interface.c
@@ -13,7 +13,8 @@ char	*get_instructions_line(t_mode mode, int fd)
 	}
 	else
 		nb_bits_read = get_next_line(fd, &line);
-	if (!line || (NON_INTERACTIVE && nb_bits_read <= 0))
+	DEBUG(printf("nb_bits_read = %i\n", nb_bits_read);)
+	if (!line || (mode == NON_INTERACTIVE && nb_bits_read <= 0))
 	{
 		free(line);
 		return (NULL);
