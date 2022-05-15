56d6b77c46298a092db29bbe845e9662eefd8721
Knulpinette <ophelie.surcouf@gmail.com>
2022-05-15 18:21:38 +0100

    uncommented

--- srcs/executing/exec_redirs.c
+++ srcs/executing/exec_redirs.c
@@ -53,8 +53,8 @@ static int	exec_redirs_in(t_cmd_table *cmd, int i)
 {
 	if (cmd->fd_in != 0 && cmd->is_infile_tmp)
 	{
-		//if (unlink(cmd->infile_tmp) == -1)
-		//	error_and_exit(UNLINK_FAIL);
+		if (unlink(cmd->infile_tmp) == -1)
+			error_and_exit(UNLINK_FAIL);
 		free(cmd->infile_tmp);
 		cmd->infile_tmp = NULL;
 		cmd->is_infile_tmp = 0;
