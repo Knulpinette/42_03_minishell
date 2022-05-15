ae5fb5b5bec49f172ee1438610930b7d6a78a739
Knulpinette <ophelie.surcouf@gmail.com>
2022-05-15 17:34:58 +0100

    changed quotes into two files

--- srcs/parsing/quotes.c
+++ srcs/parsing/quotes.c
@@ -34,42 +34,6 @@ char	check_quote(char letter, char quote)
 }
 
 /*
-** check_valid_quote
-**
-** Here we are checking that the quote we entered is a closed
-**	quote and not an open one.
-**
-*/
-
-char	check_valid_quote(char *text, char quote)
-{
-	if (ft_strchr((text + 1), quote))
-		return (quote);
-	return (0);
-}
-
-/*
-** there_is_quote
-**
-** We check there is a closed quote in a given string.
-**
-*/
-
-bool	there_is_quote(char *text, char quote)
-{
-	int		i;
-
-	i = 0;
-	while (text[i])
-	{
-		if (text[i] == quote && ft_strchr((text + i + 1), quote))
-			return (true);
-		i++;
-	}
-	return (false);
-}
-
-/*
 ** get_nb_quotes
 **
 **	Here we check how many closed quotes there are.
@@ -117,19 +81,13 @@ int	get_nb_quotes(char *text)
 
 // FUCTION HAS MORE THAN 25 LINES !!
 
-char	*remove_quotes(char *text)
+static void	remove_quotes_core(char *text, char **temp, char quote)
 {
-	char	*temp;
-	char	quote;
-	int		len_token;
-	int		i;
-	int		j;
+	int	i;
+	int	j;
 
-	len_token = ft_strlen(text) - get_nb_quotes(text);
-	temp = calloc_or_exit(sizeof(char), len_token + 1);
 	i = 0;
 	j = 0;
-	quote = 0;
 	while (text[i])
 	{
 		if (text[i] == DBL_QUOTE || text[i] == SGL_QUOTE)
@@ -137,14 +95,26 @@ char	*remove_quotes(char *text)
 		if (quote && text[i++])
 		{
 			while (text[i] && text[i] != quote)
-				temp[j++] = text[i++];
+				temp[0][j++] = text[i++];
 			quote = 0;
 			i++;
 		}
 		else
-			temp[j++] = text[i++];
+			temp[0][j++] = text[i++];
 	}
-	temp[j] = 0;
+	temp[0][j] = 0;
+}
+
+char	*remove_quotes(char *text)
+{
+	char	*temp;
+	char	quote;
+	int		len_token;
+
+	len_token = ft_strlen(text) - get_nb_quotes(text);
+	temp = calloc_or_exit(sizeof(char), len_token + 1);
+	quote = 0;
+	remove_quotes_core(text, &temp, quote);
 	free(text);
 	return (temp);
 }
