1c5ceca0e749bc87c11ffaf8a537892b1f79b0a2
Sara Vieira <svieira@student.42lisboa.com>
2022-05-23 12:35:56 +0100

    made my builtin command comparison more bullet proof

--- srcs/executing/exec_cmd_builtin.c
+++ srcs/executing/exec_cmd_builtin.c
@@ -14,32 +14,41 @@
 
 int	is_builtin(char *cmd_name)
 {
-	if (ft_strncmp(cmd_name, "cd", 2) == 0
-		|| ft_strncmp(cmd_name, "pwd", 3) == 0
-		|| ft_strncmp(cmd_name, "env", 3) == 0
-		|| ft_strncmp(cmd_name, "echo", 4) == 0
-		|| ft_strncmp(cmd_name, "export", 6) == 0
-		|| ft_strncmp(cmd_name, "unset", 5) == 0
-		|| ft_strncmp(cmd_name, "exit", 4) == 0)
+	if ((ft_strncmp(cmd_name, "cd", 2) == 0 && ft_strlen(cmd_name) == 2)
+		|| ((ft_strncmp(cmd_name, "pwd", 3) == 0
+				|| ft_strncmp(cmd_name, "env", 3) == 0)
+			&& ft_strlen(cmd_name) == 3)
+		|| ((ft_strncmp(cmd_name, "echo", 4) == 0
+				|| ft_strncmp(cmd_name, "exit", 4) == 0)
+			&& ft_strlen(cmd_name) == 4)
+		|| (ft_strncmp(cmd_name, "unset", 5) == 0 && ft_strlen(cmd_name) == 5)
+		|| (ft_strncmp(cmd_name, "export", 6) == 0 && ft_strlen(cmd_name) == 6))
 		return (1);
 	return (0);
 }
 
 int	exec_builtin(t_minishell *minishell, t_cmd_table *cmd)
 {
-	if (ft_strncmp(cmd->cmd_name, "cd", 2) == 0)
+	if (ft_strncmp(cmd->cmd_name, "cd", 2) == 0
+		&& ft_strlen(cmd->cmd_name) == 2)
 		return (cd(cmd));
-	if (ft_strncmp(cmd->cmd_name, "pwd", 3) == 0)
+	if (ft_strncmp(cmd->cmd_name, "pwd", 3) == 0
+		&& ft_strlen(cmd->cmd_name) == 3)
 		return (pwd(cmd));
-	if (ft_strncmp(cmd->cmd_name, "env", 3) == 0)
+	if (ft_strncmp(cmd->cmd_name, "env", 3) == 0
+		&& ft_strlen(cmd->cmd_name) == 3)
 		return (env(cmd, minishell));
-	if (ft_strncmp(cmd->cmd_name, "echo", 4) == 0)
+	if (ft_strncmp(cmd->cmd_name, "echo", 4) == 0
+		&& ft_strlen(cmd->cmd_name) == 4)
 		return (echo(cmd));
-	if (ft_strncmp(cmd->cmd_name, "export", 6) == 0)
+	if (ft_strncmp(cmd->cmd_name, "export", 6) == 0
+		&& ft_strlen(cmd->cmd_name) == 6)
 		return (export(cmd, minishell));
-	if (ft_strncmp(cmd->cmd_name, "unset", 5) == 0)
+	if (ft_strncmp(cmd->cmd_name, "unset", 5) == 0
+		&& ft_strlen(cmd->cmd_name) == 5)
 		return (unset(cmd, minishell));
-	if (ft_strncmp(cmd->cmd_name, "exit", 4) == 0)
+	if (ft_strncmp(cmd->cmd_name, "exit", 4) == 0
+		&& ft_strlen(cmd->cmd_name) == 4)
 		return (exit_cocoshell(cmd, minishell));
 	return (-1);
 }
